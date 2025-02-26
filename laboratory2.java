de3a66ca07692328d8a5846e3f54a9f0fbc2f875
Nassri Lakiman <lakimannassri@bscs@plmun.edu.ph>
2025-02-26 21:46:17 +0800

    nass

---
+++ laboratory2.java
@@ -0,0 +1,15 @@
+public class laboratory2 {
+    public static void main (String[] args ) {
+        int studentsAge = 18;
+        double itemsPrice = 30.99;
+        String studentsName = "Nasser";
+        int counter = 13;
+        int Counter = 20;
+        System.out.println ("Student Age:" + studentsAge);
+        System.out.println ("items Price: $" + itemsPrice);
+        System.out.println ("students Name:" + studentsName);
+        System.out.println ("counter:" + counter);
+        System.out.println ("Counter (Different Case):" + Counter);
+    
+    }
+}
