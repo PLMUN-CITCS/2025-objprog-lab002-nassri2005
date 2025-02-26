c7cfef9a95b5215eb36639587a5f615954b8257e
Nassri Lakiman <lakimannassri@bscs@plmun.edu.ph>
2025-02-26 23:35:04 +0800

    nass2

--- VariablesAndCaseSensitivity.java
+++ VariablesAndCaseSensitivity.java
@@ -1,15 +1,17 @@
-public class VariablesAndCaseSensitivity {
-    public static void main (String[] args ) {
-        int studentsAge = 20;
-        double itemsPrice = 29.99;
-        String studentsName = "Alice";
+public class VariablesAndCaseSensitivity
+{
+    public static void main(String[] args)
+    {
+        int studentAge = 15;
+        double intemPrice = 29.99;
+        String studentName ="Alice";
         int counter = 10;
         int Counter = 20;
-        System.out.println ("Student Age: " + studentsAge);
-        System.out.println ("items Price: $" + itemsPrice);
-        System.out.println ("students Name: " + studentsName);
-        System.out.println ("counter: " + counter);
-        System.out.println ("Counter (Different Case): " + Counter);
-    
+
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Item Price: $" + intemPrice);
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Counter: " + counter);
+        System.out.println("Counter (Different Case): " + Counter);
     }
 }
